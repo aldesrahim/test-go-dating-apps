3c2762f817106a4b45beccc4394a295617b16a18
aldesrahim <aldesrahim@gmail.com>
2024-12-07 10:49:48 +0700

    feat: add env loading

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"github.com/joho/godotenv"
+	"os"
+)
+
 func main() {
-	//
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+
+	requiredVariables := []string{
+		"APP_HOST",
+		"JWT_SECRET",
+		"DB_HOST",
+		"DB_PORT",
+		"DB_USER",
+		"DB_PASSWORD",
+		"DB_NAME",
+	}
+
+	for _, variable := range requiredVariables {
+		if os.Getenv(variable) == "" {
+			panic("Missing environment variable: " + variable)
+		}
+	}
 }
