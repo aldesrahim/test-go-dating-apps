68255140d9d0a272981629a9f5ccf2b334db265d
aldesrahim <aldesrahim@gmail.com>
2024-12-09 17:13:54 +0700

    feat: add product periods into product model
    remove product relation in product period to prevent circular reference when using Preload

--- models/productPeriod.go
+++ models/productPeriod.go
@@ -5,7 +5,6 @@ import "time"
 type ProductPeriod struct {
 	ID            uint      `gorm:"primarykey" json:"id"`
 	ProductID     uint      `json:"product_id"`
-	Product       Product   `json:"product"`
 	MonthlyPeriod uint      `json:"monthly_period"`
 	Price         float64   `json:"price"`
 	CreatedAt     time.Time `json:"created_at"`
