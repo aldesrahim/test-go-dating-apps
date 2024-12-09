1144f778b316b7bd7fb7dbbb8dfb6460c123df28
aldesrahim <aldesrahim@gmail.com>
2024-12-09 14:58:35 +0700

    refactor: remove user's interests complexity
    by removing these 2 tables, and just using a single column "interest" in users table

--- models/user.go
+++ models/user.go
@@ -1,25 +1,35 @@
 package models
 
 import (
+	"aldesrahim/test-go-dating-apps/utils"
+	"gorm.io/gorm"
 	"time"
 )
 
 type User struct {
-	ID            uint           `gorm:"primarykey" json:"id"`
-	Email         string         `gorm:"uniqueIndex" json:"email"`
-	PasswordHash  string         `gorm:"type:text" json:"-"`
-	Bio           *string        `gorm:"type:text" json:"bio"`
-	DOB           *time.Time     `json:"dob"`
-	Location      *string        `gorm:"type:text" json:"location"`
-	Age           *uint16        `json:"age"`
-	Name          *string        `json:"name"`
-	Gender        *string        `json:"gender"`
-	Interests     []Interest     `gorm:"many2many:user_interests;" json:"interests"`
-	UserInterests []UserInterest `json:"user_interests"`
-	CreatedAt     time.Time      `json:"created_at"`
-	UpdatedAt     time.Time      `json:"updated_at"`
+	ID           uint       `gorm:"primarykey" json:"id"`
+	Email        string     `gorm:"uniqueIndex" json:"email"`
+	PasswordHash string     `gorm:"type:text" json:"-"`
+	Bio          *string    `gorm:"type:text" json:"bio"`
+	DOB          *time.Time `json:"dob"`
+	Location     *string    `gorm:"type:text" json:"location"`
+	Age          *uint16    `json:"age"`
+	Name         *string    `json:"name"`
+	Gender       *string    `json:"gender"`
+	Interest     *string    `json:"interest"`
+	CreatedAt    time.Time  `json:"created_at"`
+	UpdatedAt    time.Time  `json:"updated_at"`
 }
 
 func (u *User) IsProfileCompleted() bool {
 	return u.Name != nil
+}
+
+func (u *User) BeforeSave(tx *gorm.DB) (err error) {
+	if u.DOB != nil {
+		age := uint16(utils.GetAgeFromDate(*u.DOB))
+		u.Age = &age
+	}
+
+	return
 }
--- models/userInterest.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type UserInterest struct {
-	UserID     uint   `gorm:"primaryKey" json:"user_id"`
-	InterestID uint   `gorm:"primaryKey" json:"interest_id"`
-	Value      string `json:"value"`
-}
