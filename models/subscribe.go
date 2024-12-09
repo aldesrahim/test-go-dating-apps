4726199f1fe2a596d51b2871546db736d9d017bf
aldesrahim <aldesrahim@gmail.com>
2024-12-09 18:10:56 +0700

    refactor: update subscribe relations to nullable

--- models/subscribe.go
+++ models/subscribe.go
@@ -3,17 +3,17 @@ package models
 import "time"
 
 type Subscribe struct {
-	ID              uint          `gorm:"primarykey" json:"id"`
-	UserID          uint          `json:"user_id"`
-	User            User          `json:"user"`
-	PaymentID       uint          `json:"payment_id"`
-	Payment         Payment       `json:"payment"`
-	ProductID       uint          `json:"product_id"`
-	Product         Product       `json:"product"`
-	ProductPeriodID uint          `json:"product_period_id"`
-	ProductPeriod   ProductPeriod `json:"product_period"`
-	StartDate       time.Time     `json:"start_date"`
-	EndDate         time.Time     `json:"end_date"`
-	CreatedAt       time.Time     `json:"created_at"`
-	UpdatedAt       time.Time     `json:"updated_at"`
+	ID              uint           `gorm:"primarykey" json:"id"`
+	UserID          uint           `json:"user_id"`
+	User            *User          `json:"user"`
+	PaymentID       uint           `json:"payment_id"`
+	Payment         *Payment       `json:"payment"`
+	ProductID       uint           `json:"product_id"`
+	Product         *Product       `json:"product"`
+	ProductPeriodID uint           `json:"product_period_id"`
+	ProductPeriod   *ProductPeriod `json:"product_period"`
+	StartDate       time.Time      `json:"start_date"`
+	EndDate         time.Time      `json:"end_date"`
+	CreatedAt       time.Time      `json:"created_at"`
+	UpdatedAt       time.Time      `json:"updated_at"`
 }
