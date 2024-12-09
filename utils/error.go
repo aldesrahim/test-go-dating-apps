bacd7be8a44d8553699fce667838578b85fd6285
aldesrahim <aldesrahim@gmail.com>
2024-12-09 10:44:59 +0700

    refactor: rename error functions

--- utils/error.go
+++ utils/error.go
@@ -1,7 +1,15 @@
 package utils
 
-import "strings"
+import (
+	"errors"
+	"gorm.io/gorm"
+	"strings"
+)
 
-func ErrorDBIsDuplicate(err error) bool {
+func ErrDBIsDuplicate(err error) bool {
 	return strings.HasPrefix(err.Error(), "Error 1062 (23000): Duplicate entry")
+}
+
+func ErrDBIsNotFound(err error) bool {
+	return errors.Is(err, gorm.ErrRecordNotFound)
 }
